2626d2e4c45cfe13ca5b15020f40812ad0872bdd
Vishwanath Krishnappanavar <Vishwanath.Krishnappanavar@infostretch.com>
2020-03-18 11:52:09 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+import Header from './components/Header';
+import {uuid} from 'uuidv4';
+
+const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Eggs'},
+    {id: uuid(), text: 'Bread'},
+    {id: uuid(), text: 'Juice'},
+  ]);
+  return (
+    <View style={styles.container}>
+      <Header />
+      <FlatList data={items} renderItem={()} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // paddingTop: 60,
+  },
+});
+
+export default App;
